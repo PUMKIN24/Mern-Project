69a145b8f3cbb313c134d6eccc679221ecaf7245
PUMKIN24 <jer2ymatheww@gmail.com>
2022-08-10 12:27:53 -0700

    nodemon added and routes for pages created

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req,res)=>{
-    res.send('hello world from server')
-})
+    res.send(`hello world from server`);
+});
 
 
-app.listen(3000,()=>{
-    console.log("server is spinning")
-})
+
+app.get('/about', (req,res)=>{
+    res.send(`hello About world from server`);
+});
+
+
+
+app.get('/contact', (req,res)=>{
+    res.send(`hello Contact world from server`);
+});
+
+
+
+app.get('/signin', (req,res)=>{
+    res.send(`hello Login world from server`);
+});
+
+
+
+app.get('/signup', (req,res)=>{
+    res.send(`hello Registration world from server`);
+});
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("server is spinning");
+});
