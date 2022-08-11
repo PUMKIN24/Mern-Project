f74b5fece0dde69688e62641e45b90a9856ad150
PUMKIN24 <jer2ymatheww@gmail.com>
2022-08-11 16:45:46 -0700

    user model created

--- server/app.js
+++ server/app.js
@@ -1,46 +1,49 @@
+
+const dotenv = require('dotenv')
 const mongoose = require("mongoose")
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 
-//Mongodb Connection 
-
-const DB = "mongodb+srv://jerrymathew:jerry@cluster0.d8rajeo.mongodb.net/mernstack?retryWrites=true&w=majority"
-
-mongoose.connect(DB
-).then(()=>{
-    console.log("DB CONNECTED")
-}).catch((err) => console.log("DB ERRRRRROR"))
+//Schema
+const User = require('./model/userSchema')
 
 
-//Middleware
+//dotenv
+dotenv.config({path : './config.env'})
 
+//MONGO DB
+const DB = process.env.DATABASE
 
-
+mongoose.connect(DB).then(() => {console.log(`DB CONNECTED`)})
+.catch((err) => console.log(`DB ERROR`))                                                                                        
 
 //PAGES
 app.get('/', (req,res)=>{
-    res.send(`hello world from server`);
-});
+    res.send(`hello world from server`)
+})   
 
 app.get('/about', (req,res)=>{
-    res.send(`hello About world from server`);
-});
+    res.send(`hello About world from server`)
+})
 
 app.get('/contact', (req,res)=>{
-    res.send(`hello Contact world from server`);
-});
+    res.send(`hello Contact world from server`)
+})
 
 app.get('/signin', (req,res)=>{
-    res.send(`hello Login world from server`);
-});
+    res.send(`hello Login world from server`)
+})
 
 app.get('/signup', (req,res)=>{
-    res.send(`hello Registration world from server`);
-});
+    res.send(`hello Registration world from server`)
+})
 
 
 //PORT
-app.listen(3000, ()=>{
-    console.log("server is spinning");
-});
+
+const PORT = process.env.PORT
+
+app.listen(PORT, ()=>{
+    console.log("server is spinning")
+})
